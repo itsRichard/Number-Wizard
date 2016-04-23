6161bd92db9e0e9607d1debfdf74c523e925d76d
Richard <rtl@uw.edu>
2016-04-23 10:41:11 -0700

    Udemy Lesson 2-18

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -14,10 +14,18 @@ public class NumberWizard : MonoBehaviour {
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min); 
 		
+		print ("Is the number higher or lower than 500?");
+		print ("Up arrow = higher.  Down arrow = lower.  Return = equals");
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			print("Up Arrow Pressed"); }
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			print("Down Arrow Pressed"); }
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			print("Return Pressed"); }
 	}
 }
