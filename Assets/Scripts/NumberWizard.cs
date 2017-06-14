91f633df104f95072564e1d9af0d0bbd443db064
Richard <rtl@uw.edu>
2017-06-14 11:10:23 -0700

    Been a while
    Test

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -3,20 +3,15 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 	// Use this for initialization
-	int max;
-	int min;
-	int guess;
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
 	
 	void Start () {
 		StartGame ();
 	}
 	
 	void StartGame () {
-		max = 1000;
-		min = 1;
-		guess = 500;
-		
-		print ("========================");
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
 		
@@ -46,7 +41,6 @@ public class NumberWizard : MonoBehaviour {
 				NextGuess ();
 			} else if (Input.GetKeyDown(KeyCode.Return)) {
 				print("I Won!"); 
-				StartGame ();
 			}
 	}
 }
